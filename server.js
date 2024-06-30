a792dcae7ccbf610d3774b71cdda2b6c26bfc07d
kartikgarg9 <KARTIKGARG20@GMAIL.COM>
2024-06-30 23:49:58 +0530

    shift mongoDb to online server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const menu = require("./models/menu");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("Welcome to kartik hotel");
